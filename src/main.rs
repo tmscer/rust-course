7367c3c148dd3e1645c35c602f457f20d30065dc
Tomáš Černý <tmscer@gmail.com>
2024-04-28 09:34:38 +0200

    Transmute text based on second arg

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
-fn main() {
-    "Hello, world!"
-        .chars()
-        .chain(std::iter::once('\n'))
-        .for_each(|c| print!("{c}"));
+use std::{env, process::ExitCode};
+
+fn main() -> anyhow::Result<ExitCode> {
+    let mut args = env::args().take(2);
+
+    args.next().ok_or(anyhow::Error::msg("Missing 0th arg"))?;
+    let transmutation = args
+        .next()
+        .ok_or(anyhow::Error::msg("Missing transmutation arg"))?;
+
+    let transmute = get_text_transmutation(&transmutation)?;
+
+    for line in std::io::stdin().lines() {
+        let line = line?;
+        let transmuted_line = transmute(&line);
+        println!("{transmuted_line}");
+    }
+
+    Ok(ExitCode::SUCCESS)
+}
+
+fn get_text_transmutation(name: &str) -> anyhow::Result<impl Fn(&str) -> String> {
+    let func = match name {
+        "lowercase" => |s: &str| s.to_lowercase(),
+        "uppercase" => |s: &str| s.to_uppercase(),
+        "no-spaces" => |s: &str| s.replace(' ', ""),
+        "slugify" => |s: &str| slug::slugify(s),
+        _ => return Err(anyhow::Error::msg("Unknown text transmutation")),
+    };
+
+    Ok(func)
 }
