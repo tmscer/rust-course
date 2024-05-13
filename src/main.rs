372c7e056a86bb13d24016c85ba4114edad76754
Tomáš Černý <tmscer@gmail.com>
2024-05-13 18:20:19 +0200

    Overengineer function signature into `Result<String>`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::{env, process::ExitCode};
+use std::{env, io, process::ExitCode};
 
 mod simple_ops;
+
+pub trait Operation<R: io::BufRead>: Fn(R) -> anyhow::Result<String> {}
 
 fn main() -> anyhow::Result<ExitCode> {
     let mut args = env::args().take(2);
@@ -10,18 +12,37 @@ fn main() -> anyhow::Result<ExitCode> {
         .next()
         .ok_or(anyhow::Error::msg("Missing transmutation arg"))?;
 
-    let transmute = get_text_transmutation(&transmutation)?;
+    let transmute = get_text_transmutation::<io::StdinLock>(&transmutation)?;
+    let output = transmute(io::stdin().lock())?;
 
-    for line in std::io::stdin().lines() {
-        let line = line?;
-        let transmuted_line = transmute(&line);
-        println!("{transmuted_line}");
-    }
+    print!("{output}");
 
     Ok(ExitCode::SUCCESS)
 }
 
-fn get_text_transmutation(name: &str) -> anyhow::Result<impl Fn(&str) -> String> {
+fn read_into_str_op<R, F>(func: F) -> impl Fn(R) -> anyhow::Result<String>
+where
+    R: io::BufRead,
+    F: Fn(&str) -> String,
+{
+    move |reader: R| -> anyhow::Result<String> {
+        let mut result = String::new();
+
+        for line in reader.lines() {
+            let line = line?;
+            let transmutated_line = func(&line);
+
+            result.push_str(&transmutated_line);
+            result.push('\n');
+        }
+
+        Ok(result)
+    }
+}
+
+fn get_text_transmutation<R: io::BufRead>(
+    name: &str,
+) -> anyhow::Result<impl Fn(R) -> anyhow::Result<String>> {
     let func = match name {
         "lowercase" => simple_ops::lowercase,
         "uppercase" => simple_ops::uppercase,
@@ -34,5 +55,5 @@ fn get_text_transmutation(name: &str) -> anyhow::Result<impl Fn(&str) -> String>
         _ => return Err(anyhow::Error::msg("Unknown text transmutation")),
     };
 
-    Ok(func)
+    Ok(read_into_str_op(func))
 }
